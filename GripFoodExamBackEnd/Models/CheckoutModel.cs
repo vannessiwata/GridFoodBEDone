7958fe18934ee3449b1607edc5900fa843dabf49
OtakKentung <95601654+vannessiwata@users.noreply.github.com>
2023-04-17 20:31:47 +0700

    initial commit

---
+++ GripFoodExamBackEnd/Models/CheckoutModel.cs
@@ -0,0 +1,11 @@
+﻿namespace GripFoodExamBackEnd.Models
+{
+    public class CheckoutModel
+    {
+        public string Id { set; get; } = "";
+        public string Name { set; get; } = "";
+        public decimal Price { set; get; }
+        public int Qty { get; set; }
+        public decimal SubTotal { set; get; }
+    }
+}
